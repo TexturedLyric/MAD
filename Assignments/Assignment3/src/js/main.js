505d82fcbe7d2a5790369a2338f97affc1182f1b
Michael Matthews <mamatthews1@buffs.wtamu.edu>
2019-02-18 14:35:18 -0600

    form finished

--- Assignments/Assignment3/src/js/main.js
+++ Assignments/Assignment3/src/js/main.js
@@ -1,46 +1,43 @@
-var root = document.getElementById('root');
-
-var thingy = 
-
-React.createElement('div', {class: 'form-group'}, 
-    React.createElement('label', {for: 'textinput'}, "ZipCode",
-        React.createElement('input', {id: 'textinput'}, {name:'textinput'}, {type:'text'}, {placeholder='77777'}
-        )
-    )
-);
-
-React.createElement('div', {class: 'form-group'}, 
-    React.createElement('label', {for: 'textinput'}, "Pizza Type",
-        React.createElement('input', {id: 'textinput'}, {name:'textinput'}, {type:'text'}, {placeholder='pepperoni'}
-        )
-    )
-);
-
-React.createElement('div', {class: 'form-group'}, 
-    React.createElement('button', {id: 'form-group'}, "submit"
-    )
-);
-
-ReactDOM.render(thingy, root);
-/*
-
-<!-- Text input-->
-<div class="form-group">
-  <label for="textinput">Zip Code</label>  
-  <input id="textinput" name="textinput" type="text" placeholder="77777">
-  </div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label for="textinput">Pizza Type</label>  
-  <input id="textinput" name="textinput" type="text" placeholder="pepperoni">
-  </div>
-</div>
-
-<!-- Button -->
-<div class="form-group">
-   <button id="button" name="button">Submit</button>
-  </div>
-</div>
-*/
+const FormInput = (props) => {
+  return React.createElement('div', {className: "form-group form-check"}, 
+      [
+          React.createElement('label', 
+                              {
+                                  htmlFor: "exampleInputZipcode1"
+                              }, 
+                              "Zipcode:"
+          ),
+          React.createElement('input', 
+                              {
+                                  className: "form-control",
+                                  type: "code",
+                                  id: "77777",
+                                  placeholder: "77777"
+                              },
+          ),
+          React.createElement('br'),
+          React.createElement('label', 
+                              {
+                                  htmlFor: "exampleInputEmail1"
+                              }, 
+                              "Email:"
+          ),
+          React.createElement('input', 
+                              {
+                                  className: "form-control",
+                                  type: "email",
+                                  id: "exampleInputEmail1",
+                                  placeholder: "Enter email"
+                              },
+          ),
+          React.createElement('br'),
+          React.createElement('input',
+                              {
+                                className:"form-check",
+                                type: "submit",
+                                class: "btn",
+                              }
+          )
+      ]
+  );
+};
